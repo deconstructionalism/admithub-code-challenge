475b973d17883ab1947b200cb223da2b494bf2b0
Arjun Ray <deconstructionalism@gmail.com>
2021-02-16 00:30:06 -0500

    feat(client): create list item
    - default props for layout testing only
    - set all pixel sizes to `rem`, set base html `font-size`

--- client/src/components/CountryListItem.js
+++ client/src/components/CountryListItem.js
@@ -1,16 +1,42 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 /*
 Takes an image URL and a country name, and renders a
 list item that can be pinned to or removed from the
 "Selected Countries" list.
 */
-const CountryListItem = () => {
+const CountryListItem = ({ flag, name }) => {
   return (
-    <li className="list-group-item">
-      Country (Update this component in Part 1)
+    <li className="d-flex flex-row align-items-center list-group-item">
+
+      <img
+        alt={ `flag of ${name}` }
+        src={ flag }
+        style={ { width: '3rem' } }
+      />
+
+      <span class="mr-auto ml-4">{ name }</span>
+
+      <button
+        class="btn"
+      >
+        &#10133;
+      </button>
+
     </li>
   )
+}
+
+// temporary; for testing layout only
+CountryListItem.defaultProps = {
+  flag: 'https://restcountries.eu/data/mex.svg',
+  name: 'Mexico'
+}
+
+CountryListItem.propTypes = {
+  flag: PropTypes.string,
+  name: PropTypes.string
 }
 
 export default CountryListItem

6a7bc589452957ec66b3a8105ac7e0a85940de0f
Arjun Ray <deconstructionalism@gmail.com>
2021-02-16 05:30:31 -0500

    feat(client): add search functionality
    - show "no results" message when query is empty
    - debounce calling REST Countries API in response
      to changes in search query
    - show "loading" message when retreiving data
    - show `CountryListItem` components for each country
      retreived from API. If no countries are retrieved, show
      "data not found" message

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,27 +1,92 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import CountryListItem from './CountryListItem.js'
+import getCountryData from '../lib/getCountryData.js'
+import useDebounceCallback from '../lib/useDebounceCallback.js'
 
 /*
-The search bar and search results. Add a handler that makes
-a rest-countries API request on key press, and returns the
-first 5 results as <CountryListItem /> components.
-Show a loading state while API request is not resolved!
+The search bar and search results.
 */
 const Search = () => {
+
+  // STATE
+
+  const [query, setQuery] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [countryData, setCountryData] = useState([])
+
+  // EVENTS HANDLERS
+
+  // get country data from API and set it in state. if `query` is empty,
+  // do not make API call and empty country data in state
+  const handleGetCountries = async () => {
+    if (query === '') { return setCountryData([]) }
+
+    const nextCountryData = await getCountryData(query)
+    setCountryData(nextCountryData)
+  }
+
+  // update `query` state on changes to input element
+  const handleChange = ({ target: { value } }) => setQuery(value)
+
+  // EFFECTS HOOKS
+
+  // debounce changes to `query` that then trigger API requests for countries
+  // data based on `query` value
+  useDebounceCallback(query, handleGetCountries, 400)
+
+  // when `query` changes, set `isLoading` state to `true` unless `query` is
+  // empty, in which case set `isLoading` to `false`
+  useEffect(() => setIsLoading(query !== ''), [query])
+
+  // when new country data has been retreived, set `isLoading` state to `false`
+  useEffect(() => setIsLoading(false), [countryData])
+
+  // LOGIC
+
+  // generate list of countries. if results are loading, show loading message
+  const CountryList = isLoading === true
+    ? <div>Loading Results...</div>
+
+      // if there are 0 countries found, show not found message
+      : countryData.length === 0
+        ? <div>No countries found</div>
+
+        // otherwise, show list of `CountryListItem` components
+        : countryData.map(({ flag, name }, index) => (
+          <CountryListItem
+            flag={ flag }
+            key={ index }
+            name={ name }
+          />
+        ))
+
+  // search results will show only if `query` is not empty
+  const SearchResults = query !== '' && (
+    <>
+
+      <h4 className="mt-2">Search Results</h4>
+
+      <ul className="list-group pr-2">
+        { CountryList }
+      </ul>
+
+    </>
+  )
+
   return (
     <div className="flex-grow-1 m-1">
+
       <input
-        type="text"
         className="w-100"
         placeholder="Start typing a country name here"
+        onChange={ handleChange }
+        type="text"
+        value={ query }
       />
-      <h4 className="mt-2">Search Results (part 2): </h4>
-      <ul className="list-group pr-2">
-        <CountryListItem />
-        <CountryListItem />
-        <CountryListItem />
-      </ul>
+
+      { SearchResults }
+
     </div>
   )
 }

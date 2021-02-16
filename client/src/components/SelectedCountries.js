16feb295030561194c220118e953dc6864338978
Arjun Ray <deconstructionalism@gmail.com>
2021-02-16 07:17:23 -0500

    feat(client): finish selected countries component
    - consume `pinned` country data and render
     `CountryListItem` components for each

--- client/src/components/SelectedCountries.js
+++ client/src/components/SelectedCountries.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import CountryListItem from './CountryListItem.js'
 
@@ -7,15 +8,38 @@ A list of selected countries (no duplicates). Replace
 the singular <CountryListItem /> below with the list of
 all selected countries.
 */
-const SelectedCountries = () => {
+const SelectedCountries = ({
+  pinned,
+  togglePinned
+}) => {
+
+  // LOGIC
+
+  // generate list of pinned countries
+  const CountryList = pinned
+    .map((data, index) => (
+      <CountryListItem
+        data={ data }
+        isPinned={ true }
+        key={ index }
+        togglePinned={ togglePinned }
+      />
+    ))
+
   return (
     <div className="flex-grow-1">
       <h4> Selected Countries (part 3): </h4>
       <ul className="list-group pr-2">
-        <CountryListItem />
+        { CountryList }
       </ul>
     </div>
   )
+}
+
+SelectedCountries.propTypes = {
+  countryData: PropTypes.arrayOf(PropTypes.object),
+  pinned: PropTypes.arrayOf(PropTypes.string),
+  togglePinned: PropTypes.func
 }
 
 export default SelectedCountries

ebc039cb85e48000c288c4bca9e07bf0e2c55b65
Arjun Ray <deconstructionalism@gmail.com>
2021-02-16 07:08:36 -0500

    feat(client): pinned country state in app
    - handler for add/remove pinned country
    - pass handler and pinned country data down to children

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Search from './components/Search.js'
 import SelectedCountries from './components/SelectedCountries.js'
@@ -8,10 +8,40 @@ The entire app that gets rendered in the "root"
 element of the page
 */
 const App = () => {
+
+  // STATE
+
+  const [pinned, setPinned] = useState([])
+
+  // EVENT HANDLERS
+
+  // add or remove country data from `pinned` state
+  const togglePinned = (data) => {
+
+    // determine if country in `data` is pinned
+    const isPinned = pinned.find(({ name }) => name === data.name)
+
+    // remove country if in `pinned`, otherwise add it
+    const nextPinned = isPinned
+      ? pinned.filter(({ name }) => name !== data.name)
+      : [...pinned, data]
+
+    setPinned(nextPinned)
+  }
+
   return (
     <div className="row w-100 d-flex pt-2">
-      <Search />
-      <SelectedCountries />
+
+      <Search
+        pinned={ pinned }
+        togglePinned={ togglePinned }
+      />
+
+      <SelectedCountries
+        pinned={ pinned }
+        togglePinned={ togglePinned }
+      />
+
     </div>
   )
 }

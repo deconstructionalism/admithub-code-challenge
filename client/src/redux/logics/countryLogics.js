606da3d01ec4d37c10c7ddc79d16a43cee207e29
Arjun Ray <deconstructionalism@gmail.com>
2021-02-17 00:06:31 -0500

    docs(client): country logics comments

--- client/src/redux/logics/countryLogics.js
+++ client/src/redux/logics/countryLogics.js
@@ -6,20 +6,38 @@ import {
   getCountriesRejected
 } from '../actions/countryActions.js'
 
+/*
+Redux logic to get country data from REST Countries API based on sub string
+matches
+*/
 const getCountriesLogic = createLogic({
   type: GET_COUNTRIES,
+
+  // debounce incoming requests based on search query updates in `Search`
   debounce: 500,
   latest: true,
   async process({ action, axios }, dispatch, done) {
+
+    // destructure action data
     const { subString } = action.payload
 
     try {
-      const { data = []} = await axios
+
+      // get all country data that matches `subString`
+      const res = await axios
         .get(`https://restcountries.eu/rest/v2/name/${subString}`)
 
+      // destructure response
+      const { data = [] } = res
+
+      // dispatch success action
       dispatch(getCountriesFulfilled(data))
     } catch (error) {
+
+      // log error
       console.error(error)
+
+      // dispatch failure action
       dispatch(getCountriesRejected())
     } finally {
       done()

679e45dd70247348ef34a9e96eb59e3a53edf001
Arjun Ray <deconstructionalism@gmail.com>
2021-02-16 23:06:09 -0500

    refactor(server): renamed routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
+const bodyParser = require('body-parser')
 const cors = require('cors')
 const express = require('express')
 const helmet = require('helmet')
 const morgan = require('morgan')
 
-const router = require('./router.js')
+const pinnedCountriesRouter = require('./routes/pinnedCountriesRouter.js')
 
 // instantiate express server
 const app = express()
@@ -15,8 +16,11 @@ app.use(morgan('dev'))
 app.use(cors({ origin: 'http://localhost:3000' }))
 app.use(helmet())
 
+// parse request body
+app.use(bodyParser.json())
+
 // add routes
-app.use(router)
+app.use(pinnedCountriesRouter)
 
 // bind server to port
 app.listen(4000, () => console.log('listening on port 4000...'))

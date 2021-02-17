d14f9169b3f2ed165a52041819be6b31e3cf070c
Arjun Ray <deconstructionalism@gmail.com>
2021-02-17 01:36:19 -0500

    feat: run script option for client only
    - can pass `--client-only` flaf to `./run.sh` script to only
      run client and not the server for testing fallback UX

--- run.sh
+++ run.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-npm run start --prefix client & npm run server --prefix server
+
+# look for `--clien-only` positional arg
+if [[ $* == *--client-only ]]
+then
+
+  # only run client application
+  npm run start --prefix client
+else
+
+  # run client and server applications
+  npm run start --prefix client & npm run server --prefix server
+fi
